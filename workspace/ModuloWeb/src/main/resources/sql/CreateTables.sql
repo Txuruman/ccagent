e61180228ef0a9cdd7f0ad9ecd8a1cbe7124243d
agsgit <Pi8423121>
2015-09-16 17:18:57 +0200

    create table

--- workspace/ModuloWeb/src/main/resources/sql/CreateTables.sql
+++ workspace/ModuloWeb/src/main/resources/sql/CreateTables.sql
@@ -1,4 +1,5 @@
 -- IDENTITY (1, 1) clave autonumerica
+DROP TABLE "dbo"."TEST";
 CREATE TABLE "dbo"."TEST"
 (
 PERSON_ID int IDENTITY (1, 1) NOT NULL,
@@ -11,6 +12,7 @@ CONSTRAINT TEST_PK PRIMARY KEY (PERSON_ID)
 
 
 -- CCAGENT_ADM
+DROP TABLE "dbo"."direct_access";
 CREATE TABLE "dbo"."direct_access"
 (
 id int IDENTITY (1, 1) NOT NULL,
@@ -22,6 +24,7 @@ CONSTRAINT direct_access_PK PRIMARY KEY (id)
 );
 
 -- CCAGENT_AUD
+DROP TABLE "dbo"."audit";
 CREATE TABLE "dbo"."audit"
 (
 date date,
@@ -33,6 +36,20 @@ result varchar(255),
 detail varchar(255),
 CONSTRAINT audit_PK PRIMARY KEY (id)
 );
+
+-- CCAGENT_AUD
+DROP TABLE "dbo"."field_config";
+CREATE TABLE "dbo"."field_config"
+(
+id int IDENTITY (1, 1) NOT NULL,
+app varchar(255),
+identifier varchar(255),
+description varchar(255),
+visible bit,
+editable bit,
+CONSTRAINT field_config_PK PRIMARY KEY (id)
+)
+
 
 
 
