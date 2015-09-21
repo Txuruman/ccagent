06fe7869011d1aa86301d602b096951fb9f840b7
agsgit <Pi8423121>
2015-09-21 12:52:19 +0200

    users

--- workspace/ModuloWeb/src/main/resources/sql/CreateTables.sql
+++ workspace/ModuloWeb/src/main/resources/sql/CreateTables.sql
@@ -196,6 +196,18 @@ CREATE TABLE "dbo"."phone"
 
 
 
+-- CCAGENT_ADM
+DROP TABLE "dbo"."users";
+CREATE TABLE "dbo"."users"
+(
+  id int IDENTITY (1, 1) NOT NULL,
+  user_id varchar(255),
+  name varchar(255),
+  isAdmin bit,
+  CONSTRAINT phone_PK PRIMARY KEY (id)
+)
+
+
 
 
 
