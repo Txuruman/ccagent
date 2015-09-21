fa22f91c3cd30d396526a8948c159b1cd59d0804
agsgit <Pi8423121>
2015-09-21 10:02:27 +0200

    getListDirectAccess

--- workspace/ModuloWeb/src/main/java/es/securitasdirect/moduloweb/web/controller/AdminController.java
+++ workspace/ModuloWeb/src/main/java/es/securitasdirect/moduloweb/web/controller/AdminController.java
@@ -1,10 +1,13 @@
 package es.securitasdirect.moduloweb.web.controller;
 
 import es.securitasdirect.moduloweb.model.Audit;
+import es.securitasdirect.moduloweb.model.CombinationsKeys;
 import es.securitasdirect.moduloweb.service.AdminService;
 import es.securitasdirect.moduloweb.service.AuditService;
 import es.securitasdirect.moduloweb.web.dto.response.AuditResponse;
+import es.securitasdirect.moduloweb.web.dto.response.ListCombinationsKeysResponse;
 import es.securitasdirect.moduloweb.web.dto.response.SimpleResponse;
+import es.securitasdirect.moduloweb.web.dto.support.BaseResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -39,6 +42,21 @@ public class AdminController extends BaseController {
         response.setData(adminService.getFieldConfig(app));
         //TODO Respuesta?
         return response;
+    }
+
+
+    @RequestMapping(value = "listcombinationskeys", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public
+    @ResponseBody
+    BaseResponse getListDirectAccess() {
+        try{
+            ListCombinationsKeysResponse response = new ListCombinationsKeysResponse();
+            List<CombinationsKeys> list = adminService.getCombinationsKeys();
+            response.setCombinationsKeys(list);
+            return response;
+        }catch(Exception exception){
+            return processException(exception);
+        }
     }
 
 }
