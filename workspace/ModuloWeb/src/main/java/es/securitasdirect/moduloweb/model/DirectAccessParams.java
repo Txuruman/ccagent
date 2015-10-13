7d38ce76a126af806f05c7df4fd42b8c79c32e91
agsgit <Pi8423121>
2015-10-13 21:22:56 +0200

    administracion

--- workspace/ModuloWeb/src/main/java/es/securitasdirect/moduloweb/model/DirectAccessParams.java
+++ workspace/ModuloWeb/src/main/java/es/securitasdirect/moduloweb/model/DirectAccessParams.java
@@ -7,14 +7,16 @@ public class DirectAccessParams {
      */
     private Integer id;
 	private String name;
-	private String value;	
+	private String value;
+    private Integer directAccess;
 	
 	public DirectAccessParams() {}
 	
-	public DirectAccessParams(String name, String value) {
+	public DirectAccessParams(String name, String value, Integer directAccess) {
 		super();
 		this.name = name;
 		this.value = value;
+        this.directAccess=directAccess;
 	}
 
     //constructor copia de la clase DirectAccessParams
@@ -22,6 +24,7 @@ public class DirectAccessParams {
         this.id = getDirectAccessParamsResult.getId().intValue();
         this.name = getDirectAccessParamsResult.getName();
         this.value = getDirectAccessParamsResult.getValue();
+        this.directAccess = getDirectAccessParamsResult.getDirectAccess().intValue();
     }
 
     public Integer getId() {
@@ -42,39 +45,13 @@ public class DirectAccessParams {
 	public void setValue(String value) {
 		this.value = value;
 	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((value == null) ? 0 : value.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		DirectAccessParams other = (DirectAccessParams) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (value == null) {
-			if (other.value != null)
-				return false;
-		} else if (!value.equals(other.value))
-			return false;
-		return true;
-	}
+    public Integer getDirectAccess() {
+        return directAccess;
+    }
+    public void setDirectAccess(Integer directAccess) {
+        this.directAccess = directAccess;
+    }
+
 	
 	
 }
